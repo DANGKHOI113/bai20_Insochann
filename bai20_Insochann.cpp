5f1453f37f6fd8824c5033d1315860ed828b608d
MAY 06 <MAY 06@LAB2-05>
2024-11-25 15:53:33 +0700

    Add project files.

---
+++ bai20_Insochann.cpp
@@ -0,0 +1,26 @@
+// bai20_Insochann.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+using namespace std;
+int main()
+{
+	int so;
+	cout << "nhap so:"; cin >> so;
+	int dem = 0;
+	for (int m = 1; m <= so; m++)
+	{
+		dem = 0;
+		for (int i = 1; i <= so; i++)
+		{
+			if (m % i == 0)
+			{
+				dem++;
+			}
+		}
+		if (dem == 2)
+		{
+			cout << m << " ";
+		}
+	}
+	return 0;
+}
